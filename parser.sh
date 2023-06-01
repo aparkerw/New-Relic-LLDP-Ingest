3af1c840a6a88c6fc13f13ff7de580dd4a49832e
Adam Parker <adamparker@newrelic.com>
2023-06-01 16:30:07 -0400

    updating the parser and the readme

--- parser.sh
+++ parser.sh
@@ -16,3 +16,30 @@ KEY_LINES=$( echo "$KEY_LINES" | sed -e 's/^[ \t]*//')
 
 # echo "$KEY_LINES" | grep -E '(--)+' 
 echo "$KEY_LINES" | uniq
+
+# an a
+FLEX_LINES=()
+
+
+NODE_DATA=""
+IFS=$'\n'
+for var in $KEY_LINES
+do
+#   FLEX_LINES+=$var
+    if [ "$var" = "-------------------------------------------------------------------------------" ]; then
+        FLEX_LINES+=($NODE_DATA)
+        # NODE_DATA=()
+    else
+        echo "Is this a node we want to have $var"
+        # $var = (sed -e '^')
+        # NODE_DATA+=("$var||")
+        NODE_DATA+="$var||"
+        echo "${#NODE_DATA[@]}"
+    fi
+done
+
+echo "*********"
+
+for node in ${FLEX_LINES[@]}; do
+    echo "$node"
+done

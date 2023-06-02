b52801a68e93a0a31d835b030418481858fde434
Adam Parker <adamparker@newrelic.com>
2023-06-02 08:31:04 -0400

    updated parser to support files without termination lines

--- parser.sh
+++ parser.sh
@@ -2,13 +2,21 @@
 
 # The fields to look for in our output
 FIELDS=("Interface" "PortID" "SysName" "TTL")
+# the dividing line between entities/nodes
+ENTITY_DIVIDER="-------------------------------------------------------------------------------"
+# field divider string
+FIELD_DIVIDER="||"
 
 PIPED_INPUT=""
+
 
 # read all the lines of piped input
 while IFS= read line; do
     PIPED_INPUT+=$line$'\n'
 done
+
+# add a guaranteed final entity divider so the last node can be captured
+PIPED_INPUT+=$ENTITY_DIVIDER$'\n'
 
 
 FIELDS_REGEX=""
@@ -16,21 +24,22 @@ for FIELD_NAME in ${FIELDS[@]}; do
     FIELDS_REGEX+="$FIELD_NAME|"
 done
 
+# only process lines we care about, drop the rest
 KEY_LINES=$( echo "$PIPED_INPUT" | grep -E "$FIELDS_REGEX------")
 # trim whitespaces
 KEY_LINES=$( echo "$KEY_LINES" | sed -e 's/^[ \t]*//')
 
-# an a
+# a variable to store our flex-friendly entries
 FLEX_LINES=()
 
 IFS=$'\n'
 for LINE in $KEY_LINES
 do
-#   FLEX_LINES+=$var
-    if [ "$LINE" == "-------------------------------------------------------------------------------" ]; then
+    if [ "$LINE" == "$ENTITY_DIVIDER" ]; then
+        # variable to store our concatinated string values
         LINE_VALUES=""
         for FIELD_NAME in ${FIELDS[@]}; do
-            LINE_VALUES+=${!FIELD_NAME}"||"
+            LINE_VALUES+=${!FIELD_NAME}"$FIELD_DIVIDER"
         done
         FLEX_LINES+=($LINE_VALUES)
         # NODE_DATA=()
@@ -54,7 +63,7 @@ done
 #####
 
 # Print out the header fields
-HEADERS=$(printf "||%s" "${FIELDS[@]}")
+HEADERS=$(printf "$FIELD_DIVIDER%s" "${FIELDS[@]}")
 echo ${HEADERS:2}
 
 # Print out the body for flex parsing
